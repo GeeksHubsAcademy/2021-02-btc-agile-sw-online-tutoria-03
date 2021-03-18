d393b36a641d26c131d8f4a4aec2b1aa711f28b7
vicboma1 <victorbolinchesmarin@gmail.com>
2021-03-18 18:32:47 +0100

    test passed & refactors SOLID

--- test/suite.test.ts
+++ test/suite.test.ts
@@ -1,34 +1,78 @@
-import { _Animal }  from "../src/es/geekshubs/academy/kata3/Animals"
-import { _Horse }  from "../src/es/geekshubs/academy/kata3/Animals"
-import { _Snake }  from "../src/es/geekshubs/academy/kata3/Animals"
+import { RepositoryImpl } from './../src/es/geekshubs/academy/kata3/repository/RepositoryImpl';
+import { Repository } from './../src/es/geekshubs/academy/kata3/repository/Repository';
+import { Animal } from "../src/es/geekshubs/academy/kata3/model/animal/Animal";
+import { AnimalImpl } from "../src/es/geekshubs/academy/kata3/model/animal/AnimalImpl";
+import { HorseImpl } from "../src/es/geekshubs/academy/kata3/model/horse/HoreImpl";
+import { Horse } from "../src/es/geekshubs/academy/kata3/model/horse/Horse";
+import { Snake } from "../src/es/geekshubs/academy/kata3/model/snake/Snake";
+import { SnakeImpl } from "../src/es/geekshubs/academy/kata3/model/snake/SnakeImpl";
+import { Builder } from '../src/es/geekshubs/academy/kata3/bulider/Builder';
 
 describe('Testing', function (){
 
-    test('_Animal name', function () {
+    test('Animal src name', function () {
         //Arrange
         var expected = "Animal-1";
         //Act
-        var result = new _Animal("Animal-1");
+        var result = new AnimalImpl("Animal-1");
         //Assert
-        expect(result.name).toBe(expected);
+        expect(result.getName()).toBe(expected);
     });
 
-    test('_Horse name', function () {
+    test('Animal api name', function () {
+        //Arrange
+        var expected = "Animal-1";
+        //Act
+        var result : Animal = new AnimalImpl("Animal-1");
+        //Assert
+        expect(result.getName()).toBe(expected);
+    });
+
+    test('Horse src name', function () {
         //Arrange
         var expected = "Horse-1";
         //Act
-        var result = new _Horse("Horse-1");
+        var result = new HorseImpl("Horse-1");
         //Assert
-        expect(result.name).toBe(expected);
+        expect(result.getName()).toBe(expected);
     });
 
-    test('_Snake name', function () {
+    test('Horse api name', function () {
+        //Arrange
+        var expected = "Galloping...Horse-1 moved 45m.";
+        //Act
+        var result : Horse = new HorseImpl("Horse-1");
+        //Assert
+        expect(result.move()).toBe(expected);
+    });
+
+    test('Snake src name', function () {
         //Arrange
         var expected = "Snake-1";
         //Act
-        var result = new _Snake("Snake-1");
+        var result = new SnakeImpl("Snake-1");
         //Assert
-        expect(result.name).toBe(expected);
+        expect(result.getName()).toBe(expected);
     });
+
+    test('Snake api name', function () {
+        //Arrange
+        var expected = "Slithering...Snake-1 moved 30m.";
+        //Act
+        var result: Snake = new SnakeImpl("Snake-1");
+        //Assert
+        expect(result.move()).toBe(expected);
+    });
+
+
+    test('Repository api getList', function () {
+        //Arrange
+        var expected = new Builder().list;
+        //Act
+        var result : Repository  = new RepositoryImpl(new Builder().list);
+        //Assert
+        expect(result.getList()).toEqual(expected);
+    });
+
 
 });
